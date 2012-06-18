796dd77320ccb108a753f46f9ff17fd730ce38be
James Henderson <james@pcr.me.uk>
2012-06-18 20:34:12 +0100

    Inlined the reduction-fn

--- src/graph_zip/core.clj
+++ src/graph_zip/core.clj
@@ -19,12 +19,16 @@
    nil ;;This is the make-node function, not entirely sure what this does...
    [nil root-subject]))
 
-(defn reduction-fn [map {:keys [subject property object]}]
-  (assoc map subject
-         (if-let [existing-props (find map subject)]
-           (cons (val existing-props) [[property object]])
-           [property object])))
-
 ;; statements :: [{:subject :property :object}]
 (defn build-graph-map [statements]
-  (reduce reduction-fn (hash-map) statements))
+  (reduce
+   #(if-let [{:keys [subject property object]} %2]
+             (assoc %1 subject
+                    (if-let [existing-props (find %1 subject)]
+                      (cons (val existing-props) [[property object]])
+                      [property object]))
+             %1)
+      (hash-map)
+      statements))
+
+  
