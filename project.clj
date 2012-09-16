971a086398e1785d017a5b2730f0119fcfc6a95c
James Henderson <james@pcr.me.uk>
2012-09-16 13:46:45 +0100

    Added to-datalog-db function

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject graph-zip "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/data.zip "0.1.0"]])
